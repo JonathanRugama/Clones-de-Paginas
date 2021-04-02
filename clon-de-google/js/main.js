076ebed159bcb1fb7a187ad31d840fba32c8e139
Jonathan <jrugama96@gmail.com>
2021-04-01 21:03:46 -0600

    Se añade algunos cambios en el archivo javascript con el objetivo de añadir un comportamiento al buscador

--- clon-de-google/js/main.js
+++ clon-de-google/js/main.js
@@ -1,11 +1,7 @@
 const input = document.querySelector(".buscador__container--ipt");
 const container = document.querySelector(".buscador__container");
 const body = document.querySelector("body");
-/* input.addEventListener('click', function(){
-   
-   container.classList.add('active');
- 
-}); */
+
 
 body.addEventListener('click', function(event){
    verifyClick(event.target);
